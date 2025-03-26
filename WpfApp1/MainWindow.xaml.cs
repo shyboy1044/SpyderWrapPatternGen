99543bb21979116ae6341d2bf2ab139ab98e8546
shyboy1044 <shyboy1044@proton.me>
2025-03-26 15:43:32 -0400

    init commit, completed UI

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This is a message box!", "Message Box Title", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This is a message box!", "Message Box Title", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void BtnSaveNew_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This is a message box!", "Message Box Title", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        
+        // Allow numbers and only one demical point
+        private void NumberOnlyTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            string fullText = textBox.Text.Insert(textBox.SelectionStart, e.Text);
+
+            // Regex: Allow numbers and single demical point
+            e.Handled = !Regex.IsMatch(fullText, @"^(\d+\.?\d*)?$");
+        }
+
+        // Prevent pasting invalid value
+        private void NumberOnlyTextBox_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(string)))
+            {
+                string pastedText = (string)e.DataObject.GetData(typeof(string));
+
+                //Regex: Check if past data is a valid double
+                if (!Regex.IsMatch(pastedText, @"^(\d+\.?\d*)?$"))
+                {
+                    e.CancelCommand();
+                }
+            }
+            else
+            {
+                e.CancelCommand();
+            }
+        }
+    }
+}
+
+
