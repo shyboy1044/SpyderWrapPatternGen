0f2308e49fde88dbd929f9583c391800e3e7a5a9
shyboy1044 <shyboy1044@proton.me>
2025-03-27 14:52:32 -0400

    Implemented basic GCode generation

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -13,15 +13,17 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Text.RegularExpressions;
+using System.Diagnostics;
 
 namespace WpfApp1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
     public partial class MainWindow : Window
     {
-
         public MainWindow()
         {
             InitializeComponent();
@@ -34,14 +36,17 @@ namespace WpfApp1
 
         private void BtnSave_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("This is a message box!", "Message Box Title", MessageBoxButton.OK, MessageBoxImage.Information);
+      //      Generate_GCode();
+
+            MessageBox.Show($"Save Button Clicked! {Generate_GCode()[2]} dfadfd", "Message Box Title", MessageBoxButton.OK, MessageBoxImage.Information);
+
         }
 
         private void BtnSaveNew_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("This is a message box!", "Message Box Title", MessageBoxButton.OK, MessageBoxImage.Information);
         }
-        
+
         // Allow numbers and only one demical point
         private void NumberOnlyTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
@@ -70,6 +75,81 @@ namespace WpfApp1
                 e.CancelCommand();
             }
         }
+        
+        private string[] Generate_GCode()
+        {
+            
+            int DDiameter = int.Parse(NumMeasuredSize.Text);
+            float DDiameterPcg = float.Parse(NumDiameterPcg.Text)/100;
+            float CirclePlus = float.Parse(NumCircPlus.Text);
+
+            float TotalKickPcg = float.Parse(NumTotalKick.Text)/100;
+            float KickRatio = float.Parse(NumKickRatioPcg.Text)/100;
+
+            float OverWrapPcg = float.Parse(NumOverWrapPcg.Text)/100;
+            int WrapPerLayer = int.Parse(NumWrapsPerLayer.Text);
+            int TotalLayers = int.Parse(NumTotalLayers.Text);
+            /*    
+                int DDiameter = 6;
+                float DDiameterPcg = 0.91f;
+                float CirclePlus = 0.03f;
+
+                float TotalKickPcg = 0.12f;
+                float KickRatio = 0.3f;
+
+                float OverWrapPcg = 100;
+                int WrapPerLayer = 5;
+                int TotalLayers = 8;
+              */
+              // Ask client About this value!
+            float YAxisPcg = 0.98f;
+
+            double XOffSet = DDiameter * 3.1415 * DDiameterPcg * TotalKickPcg;
+            double XAxisCircle = (DDiameter * 3.1415 * DDiameterPcg) - XOffSet;
+            double YAxisCircle = XAxisCircle * YAxisPcg;
+            double YOffSet = XOffSet * KickRatio;
+
+            int LayerCounter = 0;
+            int WrapCounter = 1;
+
+            double WrapXAxisTravel, WrapYAxisTravel, KickXAxisTravel, KickYAxisTravel;
+            string[] mainWrapGCode = new string[TotalLayers * WrapPerLayer * 2];
+
+            while (LayerCounter <= TotalLayers)
+            {
+                while (WrapCounter <= WrapPerLayer)
+                {
+                    WrapXAxisTravel = XAxisCircle + CirclePlus;
+                    WrapYAxisTravel = YAxisCircle + CirclePlus;
+
+                    mainWrapGCode[WrapPerLayer * LayerCounter + WrapCounter] = $"X{WrapXAxisTravel:F3}Y{WrapYAxisTravel:F3}";
+                    WrapCounter++;
+
+                    if (WrapCounter == WrapPerLayer)
+                    {
+                        KickXAxisTravel = WrapXAxisTravel + XOffSet + (DDiameter * OverWrapPcg);
+                        KickYAxisTravel = WrapYAxisTravel + YOffSet + (DDiameter * OverWrapPcg);
+                    }
+                    else
+                    {
+                        KickXAxisTravel = WrapXAxisTravel + XOffSet;
+                        KickYAxisTravel = WrapYAxisTravel + YOffSet;
+                    }
+
+                    mainWrapGCode[WrapPerLayer * LayerCounter + WrapCounter] = $"X{KickXAxisTravel:F3}Y{KickYAxisTravel:F3}";
+
+                    WrapXAxisTravel = KickXAxisTravel + XAxisCircle + CirclePlus;
+                    WrapYAxisTravel = KickYAxisTravel + YAxisCircle + CirclePlus;
+
+                    WrapCounter++;
+                }
+                WrapCounter = 1;
+                LayerCounter++;
+            }
+
+            return mainWrapGCode;
+        }
+        
     }
 }
 
